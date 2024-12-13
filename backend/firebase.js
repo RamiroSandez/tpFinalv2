a6e978f283c6916e41f59f3770c422d0ce2e96ad
RamiroSandez <ramirosandez12@gmail.com>
2024-12-12 21:59:53 -0300

    updates

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./tp-backend-238c2-firebase-adminsdk-gk5pq-9c165daa07.json"); // Ruta relativa a tu archivo JSON
+const serviceAccount = require("./firebasedatos.json"); // Ruta relativa a tu archivo JSON
 
 // Inicializar Firebase Admin SDK
 admin.initializeApp({
--- backend/firebaseAdminConfig.js
+++ backend/firebaseAdminConfig.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Verificar si Firebase ya está inicializado antes de intentar inicializarlo nuevamente
 if (!admin.apps.length) {
   // Cargar la clave privada de Firebase desde la ruta especificada en el .env
-  const serviceAccount = require(process.env.FIREBASE_CREDENTIALS_PATH);
+  const serviceAccount = require("./firebasedatos.json");
 
   // Inicializar Firebase con nombre único
   admin.initializeApp({
